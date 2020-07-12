17eab2639a9cb7fc3379df3eed98bf4cf7425acb
Gabriel Gamboa <ovnicom@OV-LTP-AYD-10.ovnicom.local>
2020-07-12 16:09:09 -0500

    adding pro's and on-demand data

--- src/App.js
+++ src/App.js
@@ -1,24 +1,33 @@
-import React from 'react';
+import React, { Component } from 'react';
 import Cabecera from './components/Cabecera';
 import './App.css';
+import { render } from '@testing-library/react';
 
-function App() {
-  return (
-    <div className="App">
-      <Cabecera />
-      <p>
-        Edit <code>src/App.js</code> and save to reload.
-      </p>
-      <a
-        className="App-link"
-        href="https://reactjs.org"
-        target="_blank"
-        rel="noopener noreferrer"
-      >
-        Learn_React
-      </a>
-    </div>
-  );
+class App extends Component{
+  manejaClick = texto => {
+    alert(texto)
+  }
+  render(){
+    const miau = 'Bienvenido miau'
+    const guau = 'Bienvenido guau'
+    return (
+      <div className="App">
+        <Cabecera miau={miau} manejaClick={this.manejaClick} />
+        <Cabecera miau={guau} manejaClick={this.manejaClick} />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn_React
+        </a>
+      </div>
+    )
+  }
 }
 
 export default App;
--- src/components/Cabecera.jsx
+++ src/components/Cabecera.jsx
@@ -2,11 +2,17 @@ import React, { Component } from 'react';
 import logo from '../logo.svg';
 
 export default class Cabecera extends Component {
+    manejaClick = () => {
+        const{ miau, manejaClick } = this.props
+        manejaClick(miau)
+    }
+
     render() {
+        const{ miau, manejaClick } = this.props
         return(
             <header className="App-header">
-                <img src={logo} className="App-logo" alt="logo" />
-                <h1>Bienvenido a ReactJS</h1>
+                <img onClick={this.manejaClick} src={logo} className="App-logo" alt="logo" />
+                <h1 className="App-title">{miau}</h1>
             </header>
         );
     }
